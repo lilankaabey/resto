6c761244bd560c334c326a48c2118eb8f3f79a69
Lilanka Ravinath <inbox.lilanka@gmail.com>
2018-12-25 07:17:13 +0530

    Add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ItemCreateComponent } from './items/item-create/item-create.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemCreateComponent
+    ItemCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +26,8 @@ import { ItemCreateComponent } from './items/item-create/item-create.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
